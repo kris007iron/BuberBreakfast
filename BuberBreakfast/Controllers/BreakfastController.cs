be3ea9c0e03b1f0c7503a11266e32fdf3a077599
Krzysztof Mrugała <66689589+kris007iron@users.noreply.github.com>
2023-06-24 21:53:23 +0200

    Basic requests implementation in controller class

---
+++ BuberBreakfast/Controllers/BreakfastController.cs
@@ -0,0 +1,15 @@
+﻿using BuberBreakfast.Contracts.Breakfast;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberBreakfast.Controllers
+{
+    [ApiController]
+    public class BreakfastController : ControllerBase
+    {
+        [HttpPost("/breakfasts")]
+        public IActionResult CreateBreakfast(CreateBreakfastRequest request)
+        {
+            return Ok();
+        }
+    }
+}
