d9b135cb989cf0e47759851e7a45b26a3921879e
Krzysztof Mrugała <66689589+kris007iron@users.noreply.github.com>
2023-06-28 20:58:29 +0200

    Controllers explainatory comments

--- BuberBreakfast/Controllers/ApiController.cs
+++ BuberBreakfast/Controllers/ApiController.cs
@@ -2,42 +2,51 @@ using ErrorOr;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 
-namespace BuberBreakfast.Controllers;
-
-[ApiController]
-[Route("[controller]")]
-public class ApiController : ControllerBase
+namespace BuberBreakfast.Controllers
 {
-    protected IActionResult Problem(List<Error> errors)
+    [ApiController]
+    [Route("[controller]")]
+    public class ApiController : ControllerBase
     {
-        if (errors.All(e => e.Type == ErrorType.Validation))
+        // This method handles the creation of a Problem response based on a list of errors.
+        protected IActionResult Problem(List<Error> errors)
         {
-            var modelStateDictionary = new ModelStateDictionary();
-
-            foreach (var error in errors)
+            // Check if all errors are of type 'Validation'
+            if (errors.All(e => e.Type == ErrorType.Validation))
             {
-                modelStateDictionary.AddModelError(error.Code, error.Description);
+                var modelStateDictionary = new ModelStateDictionary();
+
+                // Loop through each error and add it to the ModelStateDictionary
+                foreach (var error in errors)
+                {
+                    modelStateDictionary.AddModelError(error.Code, error.Description);
+                }
+
+                // Return a ValidationProblem response with the ModelStateDictionary
+                return ValidationProblem(modelStateDictionary);
             }
 
-            return ValidationProblem(modelStateDictionary);
+            // Check if any error is of type 'Unexpected'
+            if (errors.Any(e => e.Type == ErrorType.Unexpected))
+            {
+                // Return a generic Problem response
+                return Problem();
+            }
+
+            // Get the first error from the list
+            var firstError = errors[0];
+
+            // Determine the appropriate HTTP status code based on the error type
+            var statusCode = firstError.Type switch
+            {
+                ErrorType.NotFound => StatusCodes.Status404NotFound,
+                ErrorType.Validation => StatusCodes.Status400BadRequest,
+                ErrorType.Conflict => StatusCodes.Status409Conflict,
+                _ => StatusCodes.Status500InternalServerError
+            };
+
+            // Return a Problem response with the determined status code and error description as the title
+            return Problem(statusCode: statusCode, title: firstError.Description);
         }
-
-        if (errors.Any(e => e.Type == ErrorType.Unexpected))
-        {
-            return Problem();
-        }
-
-        var firstError = errors[0];
-
-        var statusCode = firstError.Type switch
-        {
-            ErrorType.NotFound => StatusCodes.Status404NotFound,
-            ErrorType.Validation => StatusCodes.Status400BadRequest,
-            ErrorType.Conflict => StatusCodes.Status409Conflict,
-            _ => StatusCodes.Status500InternalServerError
-        };
-
-
-        return Problem(statusCode: statusCode, title: firstError.Description);
     }
 }
--- BuberBreakfast/Controllers/ErrorsController.cs
+++ BuberBreakfast/Controllers/ErrorsController.cs
@@ -4,9 +4,11 @@ namespace BuberBreakfast.Controllers
 {
     public class ErrorsController : ControllerBase
     {
+        // Endpoint for handling general errors
         [Route("/error")]
         public IActionResult Error()
         {
+            // Return a generic Problem response
             return Problem();
         }
     }
