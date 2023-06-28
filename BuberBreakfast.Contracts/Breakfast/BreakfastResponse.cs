345a7f6b5693bd4f8c8e4148aa5c26de305d9cf1
Krzysztof Mrugała <66689589+kris007iron@users.noreply.github.com>
2023-06-28 21:19:34 +0200

    Contracts comments

--- BuberBreakfast.Contracts/Breakfast/BreakfastResponse.cs
+++ BuberBreakfast.Contracts/Breakfast/BreakfastResponse.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace BuberBreakfast.Contracts.Breakfast
 {
+    //Response structure
     public record BreakfastResponse(
         Guid Id,
         string Name,
